bf2898c1859aad554ecd3aa30fabf8d9b8d2ca06
Armin Hafner <a_h82@gmx.net>
2020-02-22 21:01:37 +0100

    Updates

---
+++ precache-manifest.bdefab66ff47f9bd584dd4a16a543980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321850e4c17ac4843884abde667d5cca",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "cfd65a1b288f3f6933b9",
+    "url": "/pomodoro-clock/static/css/main.739e349d.chunk.css"
+  },
+  {
+    "revision": "2e90760c531bffb4c35f",
+    "url": "/pomodoro-clock/static/js/2.47a4f3e3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoro-clock/static/js/2.47a4f3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd65a1b288f3f6933b9",
+    "url": "/pomodoro-clock/static/js/main.4fe80d50.chunk.js"
+  },
+  {
+    "revision": "4cc8e959fe1d967d61ff",
+    "url": "/pomodoro-clock/static/js/runtime-main.c524c67b.js"
+  }
+]);
